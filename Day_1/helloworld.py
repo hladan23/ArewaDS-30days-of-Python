4a23f4ac68b83291b166caadb7a84ad2a0d9e598
Hajara Ladan <ummunauwar@gmail.com>
2023-11-30 23:04:39 +0100

    day 1 and day2 completed

---
+++ Day_1/helloworld.py
@@ -0,0 +1,50 @@
+# Exercise 1.
+# Question 2
+print(3+4)
+print(4-3)
+print(3*4)
+print(3%4)
+print(4/3)
+print(4**3)
+print(4//3)
+
+# Question 3
+print('Hajara')
+print('Muhammed Ladan')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+
+# Question 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Hajara'))
+print(type('Ladan'))
+print(type('Nigeria'))
+
+# Exercise 3
+print('-------Exercise 3--------')
+# Question 1.
+print(type(9))
+print(type(3.5))
+print(type(5.18))
+print(type(8-6j))
+print(type('Maryam'))
+print(True)
+print(type(['Toyota', 'BMW', 'Honda']))
+print(type(('name', 'Hajara', 'country')))
+print(type({'age', 'gender', 'occupation'}))
+print(type({'fruits': 'banana', 'state': 'kano'}))
+
+# Question 2.
+
+import math
+a = 2
+b = 3
+c = 10
+d = 8
+
+dt = math.sqrt((a - b)**2 + (c - d)**2)
+print(f"The Euclidian distance between (2, 3) and (10, 8) is {dt}")
---
+++ day_2/variables.py
@@ -0,0 +1,52 @@
+# Level 1
+# Day 2: 30 Days of python prodramming
+first_name = 'Hajara'
+last_name = 'Ladan'
+full_name = 'Hajara Ladan'
+country = 'Nigeria'
+city = 'Kaduna'
+age = 37
+year = 2023
+is_married = True
+is_true = True
+is_light_on = True
+# Multiple variables
+first_name, last_name, city,age, is_married = 'Hajara', 'Ladan', 'Kaduna', 37, True
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print('lenght of first_name is', len(first_name), 'and the last_name is', len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff =num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+r = 30
+area_of_circle = (22/7) * r**2
+circum_of_circle = 2 * (22/7) * r
+print(area_of_circle)
+
+r = float(input('Enter the radius: '))
+area_of_circle = (22/7) * r**2
+print(area_of_circle)
+
+first_name = input('Enter first_name: ')
+last_name = input('Enter last_name: ')
+country = input('Enter country: ')
+age = input('Enter age: ')
