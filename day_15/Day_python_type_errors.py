0e3cf540f94633c8a7f3dce3e189870071c24256
Hajara Ladan <ummunauwar@gmail.com>
2023-12-17 21:06:00 +0100

    submitted

---
+++ day_15/Day_python_type_errors.py
@@ -0,0 +1,14 @@
+#  Exc=ercises: Day 15 
+# Task: Open you python interactive shell and try all the examples covered in this section.
+
+# I was able to practised the different Error types as instructed, though I was not able to comprehend some of the terminologies but  i was able to try all the error types given. The errors include:
+# 1. SyntaxError
+# 2. NameError
+# 3. IndexError
+# 4. ModuleNotFoundError
+# 5. AttributeError
+# 6. KeyError
+# 7. TypeError
+# 8. ImportError
+# 9. ValueError
+# 10. ZeroDivisionError
